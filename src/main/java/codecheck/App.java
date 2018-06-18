242ca68b75a7ee027ab7836262eaf9a411aa9869
tie209192 <tie209192@S1706C10854-T1.tisnt.tis.co.jp>
2018-06-18 22:53:30 +0900

    aaaaa

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -4,8 +4,8 @@ public class App {
 	public static void main(String[] args) {
 		//配列に引数の内容を詰める。
 		String[] array =new String [args.length];
-		String[] fizzbuzzArray=new String [args.length / 2+1];
-		int[] kazuArray=new int [args.length / 2+1];
+		String[] fizzbuzzArray=new String [args.length -1];
+		int[] kazuArray=new int [args.length -1];
 
 
 		for (int i = 0, l = args.length; i < l; i++) {
@@ -15,10 +15,15 @@ public class App {
 		//割る数を取得する
 		int wararerukazu =Integer.parseInt(args[args.length - 1]);
 
-		for (int i = 0, l = args.length/2+1; i < l; i++) {
+		for (int i = 0, l = args.length-1; i < l; i++) {
 			String aaa = args[i];
-			kazuArray[i] = Integer.parseInt (aaa.substring(0,1));
-			fizzbuzzArray[i]= aaa.substring(2);
+
+			    int index = aaa.indexOf(":");
+
+
+
+			kazuArray[i] = Integer.parseInt (aaa.substring(0,index));
+			fizzbuzzArray[i]= aaa.substring(index+1);
 		}
 
 		 String hyouji = fizzbuzz(wararerukazu,fizzbuzzArray,kazuArray);
@@ -47,7 +52,7 @@ public class App {
 
 			return String.valueOf(wararerukazu);
 			}else {
-				if (wararerukazu % kazuArray[0]== 0 &&wararerukazu %kazuArray[1] == 0&&wararerukazu %kazuArray[2] == 0&&wararerukazu %kazuArray[3] == 0) return fizzbuzzArray[0]+fizzbuzzArray[1]+fizzbuzzArray[2]+fizzbuzzArray[3];
+			if (wararerukazu % kazuArray[0]== 0 &&wararerukazu %kazuArray[1] == 0&&wararerukazu %kazuArray[2] == 0&&wararerukazu %kazuArray[3] == 0) return fizzbuzzArray[0]+fizzbuzzArray[1]+fizzbuzzArray[2]+fizzbuzzArray[3];
 			if (wararerukazu % kazuArray[0] == 0) return fizzbuzzArray[0];
 			if (wararerukazu % kazuArray[1] == 0) return fizzbuzzArray[1];
 			if (wararerukazu % kazuArray[2] == 0) return fizzbuzzArray[2];
